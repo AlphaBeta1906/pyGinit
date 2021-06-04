ec8498b3d2c08531adb3c36cd4137bc2b1606f54
fariz <farizi1906@gmail.com>
2021-06-04 09:44:43 +0700

    updates

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
-long_description = ('README.md')
+long_description =open('README.md').read()
 setup(
     name="pyGinit",
-    version="0.1.3",
+    version="0.1.5",
     description = "a simple cli tools for automation git repository creation",
     long_description = long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AlphaBeta1906/pyGinit',
+    download_url = 'https://github.com/AlphaBeta1906/pyGinit/archive/refs/tags/v0.1.3-alpha.tar.gz',
     author="fariz",
     author_email = "farizi1906@gmail.com",
     license="MIT",
     py_modules=["main"],
     classifiers=[ 
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
@@ -21,7 +22,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='automation,tools ,cli,git,github,development',
-    install_requires=["Click", "PyInquirer", "pyGithub","Colorama","GitPython"],
+    install_requires=["Click", "PyInquirer", "pyGithub","Colorama"],
     entry_points="""
         [console_scripts]
         pyGinit= pyGinit.main:pyGinit
