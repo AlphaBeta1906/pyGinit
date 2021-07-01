836dd1f2c79be0b389805a1d924c03e6ca1ee0c7
fariz <farizi1906@gmail.com>
2021-07-01 09:07:17 +0700

    update to version 0.2.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup,find_packages
 long_description =open('README.md').read()
 setup(
     name="pyGinit",
-    version="0.1.9",
+    version="0.2.0",
     description = "a simple cli tools for automation git repository creation",
     long_description = long_description,
     packages=find_packages(),
     include_package_data=True,
-    #long_description_content_type='text/markdown',
+    long_description_content_type='text/markdown',
     url='https://github.com/AlphaBeta1906/pyGinit',
-    download_url = 'https://github.com/AlphaBeta1906/pyGinit/archive/refs/tags/v0.1.9.tar.gz',
+    download_url = 'https://github.com/AlphaBeta1906/pyGinit/archive/refs/tags/v0.2.0.tar.gz',
     author="fariz",
     author_email = "farizi1906@gmail.com",
     license="MIT",
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='automation,tools ,cli,git,github,development',
-    install_requires=["Click", "PyInquirer", "pyGithub","Colorama"],
+    install_requires=["Click", "PyInquirer","Gitpython","pyGithub","Colorama"],
     entry_points="""
         [console_scripts]
         pyGinit= pyGinit.main:pyGinit
