af31d8140be534e79021687562fe7f2147a1feeb
fariz <farizi1906@gmail.com>
2021-08-11 15:59:23 +0700

    other updates

--- pyGinit/gitCommand.py
+++ pyGinit/gitCommand.py
@@ -13,12 +13,16 @@ def execute_git(username, token, repo_name, remote_name):
     initialize git repository and push to remote
     """
     remote_name = "origin" if not remote_name else remote_name
-    url = "https://{username}:{token}@github.com/{username}/{repo_name}.git".format(
-        username=username, repo_name=repo_name.replace(" ", "-"), token=token
+    url = (
+        f'https://{username}:{token}@github.com/{username}/{repo_name.replace(" ", "-")}.git'
     )
     repo = git.Repo.init()
     repo.git.add("--all")
     repo.index.commit("initial commit")
     repo.git.push(url, "HEAD:master")
     repo.create_remote(remote_name, url)
-    print('repository url :' + f'https://github.com/{username}/{repo_name}.git')
+    print(f'remote name : {remote_name}')
+    print(
+        'repository succesfully created at :'
+        + f'https://github.com/{username}/{repo_name}.git'
+    )
