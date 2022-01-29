e03170843339c18f82aedad80995707dc31b6b59
fariz <farizi1906@gmail.com>
2022-01-29 11:57:08 +0700

    add test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from pyGinit import repo
+
+def test_add_license():
+    for v in repo.license_dict:
+        print(v)
+        assert repo.addLicense(v,test=True) == 200
+
+def test_add_gitignore():
+    assert repo.add_gitignore("Python",test=True) == 200
